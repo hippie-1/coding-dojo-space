8b0cc926fbfae8887797bb83082f5f551632fb93
nh-fintech-labs <hippie.never.dies@gmail.com>
2025-02-24 13:34:40 +0100

    small changes

--- basics/src/util/Algorithms.js
+++ basics/src/util/Algorithms.js
@@ -1,7 +1,7 @@
 
 import './util.css';
 
-//töltsünk fel egy array-t random számokkal
+//töltsünk fel egy array-t random számokkal és írassuk is ki valahová.
 
 //1.feladat: számlálás
 //számoljuk meg, hogy hány negatív szám van benne
@@ -9,13 +9,21 @@ import './util.css';
 //2.feladat: max/min keresés
 //keressük meg az array legnagyobb/legkisebb értékét
 
-//3.feladat: lineáris keresés
+//3.feladat: lineáris keresés 
 //keressünk meg egy konkrét számot
+//a konkrét elemet keressük
+//ha van a tömbben, akkor
+//-- tudjuk, hogy az értéke maga a korkrét szám, amit kerestünk
+//-- kiírhatjuk inkább azt, hogy az hányadik eleme a tömbnek
+//ha nincs benne, akkor olyan írjunk, ki, amiből kiderül, hogy nincs benne, lehet azt is, hogy az index ilyenkor -1
 
-//4.feladat: rendezés
+//4.feladat: összegzés
+//adjuk össze a tömb elemeinek értékét
+
+//5.feladat: rendezés
 //rendezzük a számokat emelkedő/csökkenő sorrendbe
 
-//5.feladat bináris keresés
+//6.feladat bináris keresés
 //keressünk meg egy konkrét számot
 
 //string algoritmusok
