23bdab2041f1813a376c2031c7e7e80e9ba3159c
Reka Vida <63508198+riverreka@users.noreply.github.com>
2025-03-11 18:39:55 +0100

    DEV-43 Set export & test separately

--- basics/src/practicing/set.js
+++ basics/src/practicing/set.js
@@ -1,6 +1,6 @@
 import { removeElement, linearSearch } from './addAndRemoveElement.js';
 
-class mySet {
+export class mySet {
     #privateArray = [];
     #maxSize = 0;
 
@@ -64,31 +64,5 @@ class mySet {
 
     getSize () {
         return this.#privateArray.length;
-    }
-}
-
-if (process.argv[1] === import.meta.filename) {
-    const newSet = new mySet(10);
-    let inputArray = [1, 5, 8, 7, 8, 9, 8, 6, 7, 10, 12, 12, 15, 18, 22, 4, 3];
-    try {
-        newSet.insertNewElements(inputArray);
-        }
-    catch (e) {
-        console.log(e.message);
-    };
-    try {
-        let elementsOfNewSet = newSet.getElements();
-        console.log(`Elements of the new set:  ${elementsOfNewSet}`);
-    }
-    catch (e) {
-        console.log(e.message);
-    }
-    try {
-        newSet.removeElem(10);
-        let setAfterRemove = newSet.getElements();
-        console.log(`Elements after removing 10:  ${setAfterRemove}`);
-    }
-    catch (e) {
-        console.log(e.message);
     }
 }
