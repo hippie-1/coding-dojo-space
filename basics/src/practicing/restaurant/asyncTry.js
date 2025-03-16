9983a9fa549f2443d80d7a700016e799f3fec02e
hippie-1 <hippie.never.dies@gmail.com>
2025-03-16 13:50:02 +0100

    DEV-100 async cooking and excercises

--- basics/src/practicing/restaurant/asyncTry.js
+++ basics/src/practicing/restaurant/asyncTry.js
@@ -1,7 +1,7 @@
-export function sleep(ms) {
+  export function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-  
+
   async function task1() {
     console.log('Task 1 started');
     for (let i=1; i<=10; i++) {
@@ -26,4 +26,65 @@ export function sleep(ms) {
     console.log('Both tasks completed');
   }
   
-  startTasks();
+ // startTasks();
+
+
+
+
+ async function someWork () {
+    let promise = new Promise(
+      resolve => {
+        console.log("I am working hard");
+        setTimeout(resolve, 2000) 
+      },
+      reject => {
+        console.log("Sorry I can not do that");
+      }
+    );
+    promise.
+      then(function () {
+        finished();
+      }).
+      catch(function () {
+        rejected();
+      });
+}
+
+function finished() {
+  console.log("I am finished, I am ready for the next task");
+}
+function rejected() {
+  console.log("I am really sorry, please assing this task to someone else")
+}
+
+await Promise.all([someWork()]);
+
+async function foodPreparation (foodId) {
+  let foodPreparationPromise = new Promise(function(myResolve, myReject) {
+    // "Producing Code" (May take some time)
+      setTimeout(myResolve, 3000) 
+      myResolve("I have finished, I am ready to do the next task"); // when successful
+      myReject();  // when error
+    });
+    
+    // "Consuming Code" (Must wait for a fulfilled Promise)
+    foodPreparationPromise.then(
+      function(value) { 
+        console.log(value);
+        /* code if successful */ 
+      },
+      function(error) {
+        console.log(error);
+         /* code if some error */ 
+      }
+    );
+}
+
+function valami() {
+  console.log("Valami");
+}
+
+//await Promise.all([foodPreparation(10)]);
+
+
+
